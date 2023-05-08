cc04a29db6241dfb139f45f2ce49e7fd3f49927a
Lucas Israel <lisraelalves92@gmail.com>
2023-05-08 17:10:41 -0300

    feat: :sparkles: prio queue test done

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,28 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.file_process import process
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    len_list = [3, 19]
+    path_list = [
+        "statics/novo_paradigma_globalizado-min.txt",
+        "statics/arquivo_teste.txt",
+    ]
+    prio_q = PriorityQueue()
+    for index, path in enumerate(path_list):
+        process(path, prio_q)
+        if index == 0:
+            assert len(prio_q.regular_priority) == 1
+        if index == 1:
+            assert len(prio_q.high_priority) == 1
+
+    for index in range(2):
+        assert prio_q.search(index)["qtd_linhas"] == len_list[index]
+
+    prio_q.dequeue()
+
+    assert len(prio_q) == 1
+
+    with pytest.raises(IndexError):
+        prio_q.search(50)
