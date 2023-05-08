381f5f3a0d64b52df5b61a487c7387fc52c14574
Lucas Israel <lisraelalves92@gmail.com>
2023-05-08 15:53:10 -0300

    fix: :sparkles: fixed a problem with txt_importer and created process

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,13 +3,10 @@ import sys
 
 
 def txt_importer(path_file):
-    new_list = list()
     if not re.search(".txt$", path_file):
         print("Formato inválido", file=sys.stderr)
     try:
         with open(path_file, "r") as file:
-            for element in file:
-                new_list.append(element.strip("\n"))
+            return file.read().split("\n")
     except FileNotFoundError:
         print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
-    return new_list
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
+    for num in range(len(instance)):
+        if instance.search(num)["nome_do_arquivo"] == path_file:
+            return
+
+    file = txt_importer(path_file)
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    instance.enqueue(new_dict)
+    print(new_dict)
 
 
 def remove(instance):
@@ -8,3 +23,9 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+if __name__ == "__main__":
+    b = Queue()
+    a = process("statics/arquivo_teste.txt", b)
+    print(a)
