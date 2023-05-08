743403eac522bff987f971905ca5b14719870dda
Lucas Israel <lisraelalves92@gmail.com>
2023-05-08 18:24:54 -0300

    feat: :sparkles: search_by_word done

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -24,7 +24,24 @@ def exists_word(word: str, instance: Queue):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    new_list = []
+    for index in range(len(instance)):
+        searched = instance.search(index)
+        text_lines = searched["linhas_do_arquivo"]
+        file_name = searched["nome_do_arquivo"]
+        occur = []
+        for index2, text_line_word in enumerate(text_lines):
+            if word.lower() in text_line_word.lower():
+                occur.append({"linha": index2 + 1, "conteudo": text_line_word})
+        if len(occur) < 1:
+            return new_list
+        new_dict = {
+            "palavra": word,
+            "arquivo": file_name,
+            "ocorrencias": occur,
+        }
+        new_list.append(new_dict)
+    return new_list
 
 
 if __name__ == "__main__":
@@ -33,5 +50,5 @@ if __name__ == "__main__":
     for num in range(20):
         print("\n")
 
-    b = exists_word("Pedro", a)
+    b = search_by_word("Pedro", a)
     print(b)
