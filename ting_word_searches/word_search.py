1c91df80619cd1ae12fca0c93ab94ed156f6ae1d
Lucas Israel <lisraelalves92@gmail.com>
2023-05-08 18:20:09 -0300

    feat: :sparkles: exists_word with O(n³)

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,16 +2,22 @@ from ting_file_management.queue import Queue
 from ting_file_management.file_process import process
 
 
-def exists_word(word, instance: Queue):
+def exists_word(word: str, instance: Queue):
     new_list = []
-    for i in range(len(instance)):
-        searched = instance.search(i)
+    for index in range(len(instance)):
+        searched = instance.search(index)
         text_lines = searched["linhas_do_arquivo"]
         file_name = searched["nome_do_arquivo"]
+        occur = []
+        for index2, text_line_word in enumerate(text_lines):
+            if word.lower() in text_line_word.lower():
+                occur.append({"linha": index2 + 1})
+        if len(occur) < 1:
+            return new_list
         new_obj = {
             "palavra": word,
             "arquivo": file_name,
-            "ocorrencias": text_lines,
+            "ocorrencias": occur,
         }
         new_list.append(new_obj)
     return new_list
@@ -24,6 +30,8 @@ def search_by_word(word, instance):
 if __name__ == "__main__":
     a = Queue()
     process("statics/nome_pedro.txt", a)
+    for num in range(20):
+        print("\n")
 
     b = exists_word("Pedro", a)
     print(b)
