0beedf79aabab2389ccc54a492cda7c45ef8a7cd
Lucas Israel <lisraelalves92@gmail.com>
2023-05-08 14:57:13 -0300

    feat: :sparkles: file_management.py done

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import re
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação!"""
+    new_list = list()
+    if not re.search(".txt$", path_file):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, "r") as file:
+            for element in file:
+                new_list.append(element.strip("\n"))
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    return new_list
+
+
+if __name__ == "__main__":
+    a = txt_importer("statics/arquivo_teste.txt")
+    print(a)
